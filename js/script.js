63773ee6980b0a8e2d8509988e3a1ca2654a30dd
Alexander <rasmygens@gmail.com>
2022-07-18 23:42:38 +0300

    FoodProject init

--- js/script.js
+++ js/script.js
@@ -1,88 +1,39 @@
-/* Задание на урок:
+document.addEventListener('DOMContentLoaded', () => {
 
-1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
-перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
-Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
+    let tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
 
-2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
-переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
-
-3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
-Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
-при помощи метода forEach вывести в консоль сообщения в таком виде:
-"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)" */
-
-const personalMovieDB = {
-    count: 0,
-    movies: {},
-    actors: {},
-    geners: [],
-    privat: false,
-
-    start: function () {
-        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-        while (personalMovieDB.count == '' || personalMovieDB.count == null ||
-            isNan(personalMovieDB.count)) {
-            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
-        }
-    },
-
-    rememerMyFilms: function () {
-        for (let i = 0; i < 2; i++) {
-            const a = prompt('Один из последних просмотренных фильмов', ''),
-                b = prompt('На сколько вы его оцениваете?', '');
-
-            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-                personalMovieDB.movies[a] = b;
-                console.log('success');
-            } else {
-                console.log('error');
-                i--;
-            }
-        }
-    },
-
-    detectPersonalLevel: function () {
-        if (personalMovieDB.count < 10) {
-            console.log('Просмотренно довольно мало фильмов');
-        } else if (10 <= personalMovieDB.count < 30) {
-            console.log('Вы классический зритель');
-        } else if (personalMovieDB.count >= 30) {
-            console.log('Вы киноман');
-        } else {
-            console.log('Произошла ошибка');
-        }
-    },
-
-    showMyDB: function (hidden) {
-        if (!hidden) {
-            console.log(personalMovieDB);
-        }
-    },
-
-    writeYourGenres: function () {
-        for (let i = 1; i <= 3; i++) {
-            const genre = prompt(`Ваш любимый жанр под номером ${i}`, '');
-            if (genre != null && genre != '') {
-                personalMovieDB.geners[i - 1] = genre;
-            }
-            else {
-                console.log('error');
-                i--;
-            }
-        }
-
-        personalMovieDB.geners.forEach((item, i) => {
-            console.log(`Любимый жанр #${i + 1} - это ${item}`);
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
         });
-    },
 
-    toggleVisibleDB: function () {
-        if (personalMovieDB.privat == false) {
-            personalMovieDB.privat = true;
-        } else {
-            personalMovieDB.privat = false;
-        }
+        tabs.forEach(tab => {
+            tab.classList.remove('tabheader__item_active');
+        });
     }
-};
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+});
