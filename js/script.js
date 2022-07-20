d66f3834aef2fe99660188596aa2829c104393f5
Alexander <rasmygens@gmail.com>
2022-07-20 19:04:30 +0300

    +npm, json-server, fetches

--- js/script.js
+++ js/script.js
@@ -289,5 +289,8 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 4000);
     }
 
+    fetch('http://localhost:3000/menu')
+    .then(data => data.json())
+    .then(res => console.log(res));
 
 });
